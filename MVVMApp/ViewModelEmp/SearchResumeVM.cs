61fe5dbc6001a069e04bfae080fd1900ba3a13c4
Дмитрий <boldyrevdmitrij614@gmail.com>
2026-03-07 06:13:40 +0300

    Расширил и немного поправил интерфейс. Добавил некотрым кнопкам команды. Создал окно с формой заполнения для работодателя

--- MVVMApp/ViewModelEmp/SearchResumeVM.cs
+++ MVVMApp/ViewModelEmp/SearchResumeVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVVMApp.Other;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,29 @@ using System.Threading.Tasks;
 
 namespace MVVMApp.ViewModelEmp
 {
-    internal class SearchResumeVM
+    class SearchResumeVM : Property
     {
+        #region Поисковая строка
+
+        private string _row;
+        public string Row { get => _row;
+            set { if (_row != value)
+                { _row = value; OnPropertyChanged(); }
+            }
+        }
+        #endregion
+        #region Кнопка стереть
+        public RelayCommand clearCommand { get; set; }
+        public void clearCommandExecute(object p) { Row = null; }
+        public bool clearCommandCanExecute(object p) { return true; }
+        #endregion
+        #region Кнопка найти
+
+        #endregion
+        public SearchResumeVM()
+        {
+            clearCommand = new RelayCommand(clearCommandExecute, clearCommandCanExecute);
+        }
+
     }
 }
--- MVVMApp/ViewModelEmp/VacancyEmpVM.cs
+++ MVVMApp/ViewModelEmp/VacancyEmpVM.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using MVVMApp.Other;
+using MVVMApp.View.EmployerUI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace MVVMApp.ViewModelEmp
 {
-    internal class VacancyEmpVM
+    class VacancyEmpVM : Property
     {
+        #region Поисковая строка
+
+        private string _row;
+        public string Row
+        {
+            get => _row;
+            set
+            {
+                if (_row != value)
+                { _row = value; OnPropertyChanged(); }
+            }
+        }
+        #endregion
+        #region Кнопка стереть
+        public RelayCommand clearCommand { get; set; }
+        public void clearCommandExecute(object p) { Row = null; }
+        public bool clearCommandCanExecute(object p) { return true; }
+        #endregion
+        #region Кнопка добавить
+        
+        public RelayCommand addVacancyCommand { get; set; }
+        public void addVacancyCommandExecute(object p) { 
+            NewVacancyForm nvc = new NewVacancyForm();
+            nvc.ShowDialog(); }
+        public bool addVacancyCommandCanExecute(object p) { return true; }
+        #endregion
+        public VacancyEmpVM() {
+
+            clearCommand = new RelayCommand(clearCommandExecute, clearCommandCanExecute);
+            addVacancyCommand = new RelayCommand(addVacancyCommandExecute, addVacancyCommandCanExecute);
+        }
     }
 }
